80993ca2f7a6d2a20823befbd5dbf6162ae4ceaa
Harshit Seksaria <37345795+letsintegreat@users.noreply.github.com>
2021-12-05 09:00:33 +0530

    Add google sign in

--- config/auth.js
+++ config/auth.js
@@ -5,5 +5,10 @@ module.exports = {
     }
     req.flash('error_msg' , 'please login to view this resource');
     res.redirect('/users/login');
+    },
+    'googleAuth' : {
+        'clientID'      : 'your-client-ID',
+        'clientSecret'  : 'your-client-secret',
+        'callbackURL'   : 'http://127.0.0.1:3100/users/google/callback'
     }
-    }
+}
--- config/passport.js
+++ config/passport.js
@@ -1,9 +1,11 @@
 const User = require('../models/user');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var configAuth = require('./auth');
 
 module.exports = function(passport){
-    passport.use(
+    passport.use('local',
         new LocalStrategy({usernameField: 'email'},(email,password,done)=>{
             //match user
             User.findOne({email:email})
@@ -32,4 +34,47 @@ module.exports = function(passport){
             done(err,user);
         })
     })
+    passport.use('google', new GoogleStrategy({
+
+        clientID        : configAuth.googleAuth.clientID,
+        clientSecret    : configAuth.googleAuth.clientSecret,
+        callbackURL     : configAuth.googleAuth.callbackURL,
+
+    },
+    function(token, refreshToken, profile, done) {
+
+        // make the code asynchronous
+        // User.findOne won't fire until we have all our data back from Google
+        process.nextTick(function() {
+
+            // try to find the user based on their google id
+            User.findOne({ 'google.id' : profile.id }, function(err, user) {
+                if (err)
+                    return done(err);
+
+                if (user) {
+
+                    // if a user is found, log them in
+                    return done(null, user);
+                } else {
+                    // if the user isnt in our database, create a new user
+                    var newUser          = new User();
+
+                    // set all of the relevant information
+                    newUser.id    = profile.id;
+                    newUser.token = token;
+                    newUser.name  = profile.displayName;
+                    newUser.email = profile.emails[0].value; // pull the first email
+
+                    // save the user
+                    newUser.save(function(err) {
+                        if (err)
+                            throw err;
+                        return done(null, newUser);
+                    });
+                }
+            });
+        });
+
+    }));
 }
